fab76e3f396762efe9f88434284b9459ba52f47b
TrashofRookie <chenjingwei432@126.com>
2022-08-24 17:11:40 +0800

    EtypeII

---
+++ narrowband_quantization.m
@@ -0,0 +1,18 @@
+function [P] = narrowband_quantization(W, W_max)
+W = abs(W);
+% W_max = max(W);
+W_norm = W / W_max;
+codebook = [1, 1/sqrt(2), 1/2, 1/sqrt(8), 1/4, 1/sqrt(32), 1/8, 1/sqrt(128)];
+P = quantize(W_norm, codebook);
+
+    
+end
+
+function ret = quantize(samples, codes)
+    codes = codes(:);
+    codes = sort(codes)';
+    midpoints = (codes(2 : end) + codes(1 : end - 1)) / 2;
+    diffs = codes(2 : end) - codes(1 : end-1);
+    ret = sum((samples(:) > midpoints) .* diffs, 2) + codes(1);
+    ret = reshape(ret, size(samples));
+end
